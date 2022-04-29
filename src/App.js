e8d2b0fddac3cfe8339b1e5ce6234c4643a6f424
inspherevicky <insphere.vicky@gmail.com>
2022-04-29 20:53:51 +0530

    filter text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,121 @@
-import logo from './logo.svg';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import './App.css';
 
 function App() {
+
+const [rowData,setRowData] = useState()
+const [filterData,setFilterData] = useState()
+const [showDropdown,setShowDropdown] = useState(false)
+
+  const onGridReady = useCallback((params) => {
+    fetch('https://www.ag-grid.com/example-assets/olympic-winners.json')
+      .then((resp) => resp.json())
+      .then((data) => {
+        setRowData(data);
+      });
+  });
+
+  useEffect(() => {
+    onGridReady()
+  },[])
+
+  const inputText =(e) => {
+    const data = rowData.filter(item =>{
+      if(e.target.value == ''){
+        return item
+      }
+      // else if(item.athlete.toLowerCase().includes(e.target.value.toLowerCase())){
+      //   return item
+      // }
+      else{
+        return item === '' || item.athlete === e.target.value
+      }
+    })
+    setFilterData(data)
+    console.log('kkkk',data);
+    console.log('kkkk111',e.target.value);
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {console.log('data',rowData)}
+      {showDropdown && <h1>filter</h1>}
+      
+      <table className="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">athlete</th>
+      <th scope="col">age</th>
+      <th scope="col"> country</th>
+      <th scope="col">date</th>
+      <th scope="col">gold</th>
+      <th scope="col">silver</th>
+      <th scope="col">bronze</th>
+      <th scope="col">sport</th>
+      <th scope="col">total</th>
+      <th scope="col">year</th>
+    </tr>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">
+      <div style={{display:'flex'}}>
+        <div class="btn-group" style={{marginRight:10}}>
+          <a class="dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" />
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">(a)Contains</a></li>
+              <li><a class="dropdown-item" href="#">a() Not Contains</a></li>
+              <li><a class="dropdown-item" href="#">= Equals</a></li>
+              <li><a class="dropdown-item" href="#">!= Not Equal</a></li>
+              <li><a class="dropdown-item" href="#">Aa Start With</a></li>
+              <li><a class="dropdown-item" href="#">aAEnd With</a></li>
+           </ul>
+        </div>
+        <input type="text" onChange={inputText} placeholder="Enter athlete"/>
+      </div>
+      </th>
+      <th scope="col">        
+        <div class="btn-group" style={{marginRight:10}}>
+          <a class="dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false" />
+            <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <li><a class="dropdown-item" href="#">Equal to</a></li>
+              <li><a class="dropdown-item" href="#">Greater Then</a></li>
+              <li><a class="dropdown-item" href="#">Less Then</a></li>
+           </ul>
+        </div>
+        <input type="text" onChange={inputText} placeholder="Enter Age"/></th>
+      <th scope="col"><input type="text" /></th>
+      <th scope="col"><input type="date" onChange={inputText}/></th>
+      <th scope="col"><input type="text" /></th>
+      <th scope="col"><input type="text" /></th>
+      <th scope="col"><input type="text" /></th>
+      <th scope="col"><input type="text" /></th>
+      <th scope="col"><input type="text" /></th>
+      <th scope="col"><input type="text" /></th>
+    </tr>
+  </thead>
+  {(!filterData ?  rowData : filterData)?.map(item => (
+  <>
+  <tbody key={item.id}>
+    <tr >
+      <th scope="row" >{item.id}</th>
+      <td>{item.athlete}</td>
+      <td>{item.age}</td>
+      <td>{item.country}</td>
+      <td>{item.date}</td>
+      <td>{item.gold}</td>
+      <td>{item.silver}</td>
+      <td>{item.bronze}</td>
+      <td>{item.sport}</td>
+      <td>{item.total}</td>
+      <td>{item.year}</td>
+    </tr>
+  </tbody>
+  
+  </>
+  ))}
+  
+</table>
     </div>
   );
 }
